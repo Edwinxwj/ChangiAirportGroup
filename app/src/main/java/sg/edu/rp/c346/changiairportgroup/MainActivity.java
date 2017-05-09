b5a6292cf4029ae9f2f0f34aaa630c8b46421790
15056215 <15056215@myrp.edu.sg>
2017-05-09 15:00:38 +0800

    Updated

--- app/src/main/java/sg/edu/rp/c346/changiairportgroup/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/changiairportgroup/MainActivity.java
@@ -2,6 +2,11 @@ package sg.edu.rp.c346.changiairportgroup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
+
