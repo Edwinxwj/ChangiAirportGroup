0bbd6397ac7a6f7bd76a68709dba9524e6e6e5d9
15056215 <15056215@myrp.edu.sg>
2017-07-19 17:21:30 +0800

    19/7/2017 - search

--- app/src/main/java/sg/edu/rp/c346/changiairportgroup/MainActivityAirTraffic.java
+++ app/src/main/java/sg/edu/rp/c346/changiairportgroup/MainActivityAirTraffic.java
@@ -5,10 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -34,17 +38,30 @@ public class MainActivityAirTraffic extends AppCompatActivity {
         gates = new ArrayList<String>();
         gates.add("E63");
         gates.add("E64");
+        gates.add("E65");
+        gates.add("E66");
+        gates.add("E67");
+        gates.add("E68");
+        gates.add("E69");
+        gates.add("E70");
+        gates.add("E71");
+        gates.add("E72");
+        gates.add("E73");
+        gates.add("E74");
+        gates.add("E75");
+
 
         aa = new ArrayAdapter(this, android.R.layout.simple_list_item_1, gates);
         lv.setAdapter(aa);
 
 
-
+        //Creating Spinner
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivityAirTraffic.this,
                 android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.spinnersTerminal));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner.setAdapter(myAdapter);
 
+        //Create on click for the list view
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -56,5 +73,30 @@ public class MainActivityAirTraffic extends AppCompatActivity {
             }
         });
 
+    }
+    //Search bar
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.SearchId);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                aa.getFilter().filter(newText);
+                return true;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
